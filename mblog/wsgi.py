bbd63a5fd62b0025b1b268d937e25647eddecaed
0624036 <0624036@nkust.edu.tw>
2021-06-21 18:01:29 +0800

    06211801

--- mblog/wsgi.py
+++ mblog/wsgi.py
@@ -6,7 +6,7 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
-
+"""
 import os
 
 from django.core.wsgi import get_wsgi_application
@@ -14,3 +14,10 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mblog.settings')
 
 application = get_wsgi_application()
+"""
+# wsgi.py
+import os
+from dj_static import Cling
+from django.core.wsgi import get_wsgi_application
+os.environ.setdefault("DJANGO_SETTINGS_MODULE","mblog.settings")
+application = Cling(get_wsgi_application())
